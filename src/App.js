4fc32adc8be2cbc28862870980148c9079731c10
salman112211 <ksalmantaj@gmail.com>
2023-03-24 11:42:48 +0500

    temprature app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 
-const App =() => {
+const App = () => {
 
-  const [tempValue, settempvalue]=useState(10);
-  const [tempColor, settempColor]=useState("cold");
+  const [tempValue, settempvalue] = useState(10);
+  const [tempColor, settempColor] = useState("cold");
 
-  const increseTemprature=()=>{
-    if(tempValue===30) return;
- const newTemprature = tempValue + 1;
-    
- if(newTemprature >=15){
+  const increseTemprature = () => {
+    if (tempValue === 30) return;
+    const newTemprature = tempValue + 1;
+
+    if (newTemprature >= 15) {
       settempColor('hot');
     };
 
@@ -19,34 +19,34 @@ const App =() => {
 
 
 
-  const DcreseTemprature=()=>{
-    if(tempValue===0) return;
-  const newTemprature = tempValue - 1;
-    if(newTemprature <15){
+  const DcreseTemprature = () => {
+    if (tempValue === 0) return;
+    const newTemprature = tempValue - 1;
+    if (newTemprature < 15) {
       settempColor('cold');
     };
 
     settempvalue(newTemprature);
   };
 
-  return(
+  return (
     <div className="app-container">
 
-<div className="temperature-display-container">
+      <div className="temperature-display-container">
 
-  <div className={`temperature-display ${tempColor}`}>{tempValue}c</div>
+        <div className={`temperature-display ${tempColor}`}>{tempValue}c</div>
 
-</div>
+      </div>
 
-<div className='button-container'>
+      <div className='button-container'>
 
-  <button onClick={()=>increseTemprature() }>+</button>
-    <button onClick={()=> DcreseTemprature()}>-</button>
+        <button onClick={() => increseTemprature()}>+</button>
+        <button onClick={() => DcreseTemprature()}>-</button>
 
 
-</div>
-     
-</div>
+      </div>
+
+    </div>
   )
 }
 
